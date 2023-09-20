86cf69bad501d5e68dfb18441ac7ae476d85347a
Hou Neavireakpong <neavireakpong.hou@gmail.com>
2023-09-20 19:19:16 +0700

    Add new API routes for books, dicussions and comments

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\DiscussionController;
 use App\Http\Controllers\UserController;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +27,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('dashboard', [UserController::class, 'dashboard']);
     Route::post('logout', [UserController::class, 'logout']);
+
+    Route::post('discussions', [DiscussionController::class, 'store']);
+    Route::patch('discussions/{dicussion}', [DiscussionController::class, 'update']);
+    Route::post('comments', [CommentController::class, 'store']);
+    Route::patch('comments/{comment}', [CommentController::class, 'update']);
+    
+    Route::post('comments/upvote/{comment}', [CommentController::class, 'upvote']);
 });
 
 Route::post('login', [UserController::class, 'index']);
 Route::post('register', [UserController::class, 'register']);
+
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{book}', [BookController::class, 'show']);
+Route::post('books', [BookController::class, 'store']);
