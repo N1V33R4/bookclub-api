dae43272e9eedbbb5602c16f54056aabc3c9cd84
Hou Neavireakpong <neavireakpong.hou@gmail.com>
2023-09-24 17:38:04 +0700

    Add local ip of react app to cors

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,http://localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,http://localhost:3000,127.0.0.1,127.0.0.1:8000,::1,http://192.168.1.52:3000',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
